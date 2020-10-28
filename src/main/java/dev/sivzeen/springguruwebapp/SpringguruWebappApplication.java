d5ef61544fe01097f9a6231fd865d80019b6f2f6
siviwe.sisusa <siviwe.sisusa@interfront.co.za>
2020-10-28 16:57:38 +0200

    Initial commit

---
+++ src/main/java/dev/sivzeen/springguruwebapp/SpringguruWebappApplication.java
@@ -0,0 +1,13 @@
+package dev.sivzeen.springguruwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringguruWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringguruWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sivzeen/springguruwebapp/SpringguruWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sivzeen.springguruwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringguruWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
